ed5476bbd0fc43e6194a661119e54aa56adeeaf4
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-01-28 22:03:19 +0530

    MVC setup

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 // const express = require("express");
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import testRoutes from "./routes/testRoutes.js";
+
+///dotenv configuration
+// dotenv.config({ path: "./" });
+dotenv.config();
 
 ///Rest Ojbject
 const app = express();
 
-///Route
+///Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//route
+app.use("/api/v1/test", testRoutes);
+
+///Home Route
 app.get("/", (req, res) => {
   return res.status(200).send("Welcome to xSpark-food by skDoDesign.");
 });
 
 ///Port
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 ///Listening
 app.listen(PORT, () => {
-  console.log("Server is running.");
+  console.log(`Node Server is running on ${PORT}`);
 });
